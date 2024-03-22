da088685d1eb29153d669ee07747fa6501a25031
Nikhil-2511 <113086602+Nikhil-2511@users.noreply.github.com>
2024-03-22 18:28:02 +0530

    Add files via upload

---
+++ Plus_cal.cpp
@@ -0,0 +1,14 @@
+5#include <iostream>
+
+using namespace std;
+
+int main() {
+    double num1, num2;
+
+    cout << "Enter two numbers to add: ";
+    cin >> num1 >> num2;
+
+    cout << "Result: " << num1 << " + " << num2 << " = " << num1 + num2;
+
+    return 0;
+} 
